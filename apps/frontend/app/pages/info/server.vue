e6d90e12b707dc0c2660e17b2d6572d6718295b5
surumeneco <128275841+surumeneco@users.noreply.github.com>
2026-09-18 10:53:18 +0900

    feat: add server basics connection details and world structure

--- apps/frontend/app/pages/info/server.vue
+++ apps/frontend/app/pages/info/server.vue
@@ -3,6 +3,41 @@
     <h2>サーバー情報</h2>
 
     <h3>基本情報</h3>
+    <p>Minecraft 26.2 paperサーバー</p>
+    <p>最大同時接続数設定：20</p>
+
+    <h4>接続情報</h4>
+    <h5>Java版</h5>
+    <p>アドレス：xplayje.mc.surumene.co</p>
+
+    <h5>Bedrock版</h5>
+    <p>アドレス：xplaybe.mc.surumene.co</p>
+    <p>ポート番号：12504</p>
+
+    <h4>ワールド構成</h4>
+    <ul>
+      <li>
+        メインサーバー
+        <ul>
+          <li>
+            メインワールド
+            <ul>
+              <li>オーバーワールド</li>
+              <li>ネザー</li>
+              <li>エンド</li>
+            </ul>
+          </li>
+          <li>
+            資源用ワールド
+            <ul>
+              <li>オーバーワールド</li>
+              <li>ネザー</li>
+              <li>エンド</li>
+            </ul>
+          </li>
+        </ul>
+      </li>
+    </ul>
 
     <h3>導入データパック</h3>
     <h4>Lithosphere</h4>
